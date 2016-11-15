18507ece3ec3644f3be215df2111d0361293a8b0
qoder <qoder@hrsoft.net>
2016-11-15 22:28:07 +0800

    增加了 matations

---
+++ src/vuex/modules/app/mutation-type.js
@@ -0,0 +1,6 @@
+/**
+ * Created by qoder on 16/11/15.
+ */
+export const CHANGE_LEFTNAV_STATE = 'CHANGE_LEFTNAV_STATE'
+export const START_LOADING = 'START_LOADING'
+export const STOP_LOADING = 'STOP_LOADING'
---
+++ src/vuex/modules/app/mutations.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qoder on 16/11/15.
+ */
+import * as types from './mutation-type'
+
+const mutations={
+
+}
